8f49f1ec94ce2c995818e2f00116577f4697c671
Ngwa Sedrick Meh <nsedrick101@gmail.com>
2022-11-21 12:46:07 +0100

    update: add configs to ease deployment

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -8,7 +8,7 @@ const darkCodeTheme = require("prism-react-renderer/themes/dracula");
 const config = {
   title: "SMSWithoutBorders",
   tagline: "Stay connected",
-  url: "https://smswithoutborders.com",
+  url: "https://smswithoutborders.github.io",
   baseUrl: "/",
   onBrokenLinks: "throw",
   onBrokenMarkdownLinks: "warn",
@@ -18,7 +18,8 @@ const config = {
   // If you aren't using GitHub pages, you don't need these.
   organizationName: "smswithoutborders", // Usually your GitHub org/user name.
   projectName: "smswithoutborders.github.io", // Usually your repo name.
-
+  deploymentBranch: "gh-pages",
+  trailingSlash: true,
   // Even if you don't use internalization, you can use this field to set useful
   // metadata like html lang. For example, if your site is Chinese, you may want
   // to replace "en" with "zh-Hans".
