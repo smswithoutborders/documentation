697e24f1090a7fadf2e4ee9b4507bc372477aa5d
mildrette <mildredfonka1@gmail.com>
2024-10-02 13:26:37 +0100

    Redesigned homepage header and improved button layout for responsiveness

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -5,22 +5,14 @@ const config = {
   title: "SMSWithoutBorders >> Documentation",
   tagline: "Stay connected",
   favicon: "img/favicon.ico",
-
-  // Set the production url of your site here
   url: "https://docs.smswithoutborders.com",
   baseUrl: "/",
-  organizationName: "SMSWithoutBorders", // Usually your GitHub org/user name.
-  projectName: "documentation", // Usually your repo name.
+  organizationName: "SMSWithoutBorders",
+  projectName: "documentation",
   deploymentBranch: "gh-pages",
   trailingSlash: false,
-  // GitHub pages deployment config.
-  // If you aren't using GitHub pages, you don't need these.
   onBrokenLinks: "ignore",
   onBrokenMarkdownLinks: "ignore",
-
-  // Even if you don't use internationalization, you can use this field to set
-  // useful metadata like html lang. For example, if your site is Chinese, you
-  // may want to replace "en" with "zh-Hans".
   i18n: {
     defaultLocale: "en",
     locales: ["en", "fr", "fa"],
@@ -57,15 +49,11 @@ const config = {
       ({
         docs: {
           sidebarPath: "./sidebars.js",
-          // Please change this to your repo.
-          // Remove this to remove the "edit this page" links.
           editUrl:
             "https://github.com/smswithoutborders/smswithoutborders.github.io/tree/main/",
         },
         blog: {
           showReadingTime: true,
-          // Please change this to your repo.
-          // Remove this to remove the "edit this page" links.
           editUrl:
             "https://github.com/smswithoutborders/smswithoutborders.github.io/tree/main/",
         },
@@ -79,7 +67,6 @@ const config = {
   themeConfig:
     /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
     ({
-      // Replace with your project's social card
       navbar: {
         title: "SMSWithoutBorders",
         logo: {
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,37 +1,42 @@
-import clsx from 'clsx';
-import Link from '@docusaurus/Link';
-import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
-import Layout from '@theme/Layout';
+import clsx from "clsx";
+import Link from "@docusaurus/Link";
+import useDocusaurusContext from "@docusaurus/useDocusaurusContext";
+import Layout from "@theme/Layout";
 import SwobLogo from "../../static/img/logo-icon-light.png";
-import styles from './index.module.css';
+import styles from "./index.module.css";
 import Translate from "@docusaurus/Translate";
 
 function HomepageHeader() {
-  const {siteConfig} = useDocusaurusContext();
+  const { siteConfig } = useDocusaurusContext();
   return (
-    <header className={clsx('hero', styles.heroBanner)}>
-      <div className="container">
-      <img
-          alt="swob logo"
+    <header className={clsx("hero", styles.heroBanner)}>
+      <div className={clsx("container", styles.headerContainer)}>
+        {/* Logo Section */}
+        <img
+          alt="Swob logo"
           className={styles.hero_img}
           src={SwobLogo}
           height={180}
           width={180}
         />
+
+        {/* Title and Tagline */}
         <h1 className={styles.hero__title}>{siteConfig.title}</h1>
-        <p className={styles.hero__subtitle}>{siteConfig.tagline}</p> 
-        <div className={styles.buttons}>
+        <p className={styles.hero__subtitle}>{siteConfig.tagline}</p>
+
+        {/* Button Group */}
+        <div className={styles.buttonGroup}>
           <Link
             className="button button--outline button--primary button--lg"
             to="/docs/contributing/introduction"
+            aria-label="Get started with iOS tutorial"
           >
-            <Translate>Get Started with Desktop</Translate>
+            <Translate>Get Started with iOS</Translate>
           </Link>
-        </div>
-        <div className={styles.buttons} style={{ marginTop: "10px" }}>
           <Link
             className="button button--outline button--primary button--lg"
             to="/docs/App Tutorial/New-Tutorial"
+            aria-label="Get started with Android tutorial"
           >
             <Translate>Get Started with Android</Translate>
           </Link>
@@ -42,11 +47,12 @@ function HomepageHeader() {
 }
 
 export default function Home() {
-  const {siteConfig} = useDocusaurusContext();
+  const { siteConfig } = useDocusaurusContext();
   return (
     <Layout
       title={`${siteConfig.title}`}
-      description="Smswithoutborders tutorail section <head />">
+      description="Smswithoutborders tutorial section"
+    >
       <HomepageHeader />
     </Layout>
   );
